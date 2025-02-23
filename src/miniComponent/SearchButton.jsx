ccba487cd014fb359d4fc9ef168fc58cfc413243
sahil saini <csesai07@gmail.com>
2025-02-23 21:12:13 +0530

    nav responsiveness and logo text font

--- src/miniComponent/SearchButton.jsx
+++ src/miniComponent/SearchButton.jsx
@@ -37,7 +37,7 @@ const SearchButton = ({ searchInput, setSearchInput, handleSearch }) => {
             debouncedHandleSearch();
           }
         }}
-        className={`outline-none inline-flex bg-transparent text-[16px]`}
+        className={`outline-none inline-flex bg-transparent w-44 text-[16px] overflow-hidden`}
       />
       <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.3rem] flex h-5 select-none items-center gap-1 rounded px-1.5 font-mono text-[10px] font-medium opacity-100 [&_span]:text-xs sm:flex">
         <span>⌘</span>K
