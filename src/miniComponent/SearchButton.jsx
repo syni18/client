27419b1657195bd591c5b03a714106047bc45539
sahil saini <csesai07@gmail.com>
2025-02-23 20:49:02 +0530

    nav responsiveness and logo text font

--- src/miniComponent/SearchButton.jsx
+++ src/miniComponent/SearchButton.jsx
@@ -24,7 +24,7 @@ const SearchButton = ({ searchInput, setSearchInput, handleSearch }) => {
         border border-[--border] bg-[--background] 
         text-[--color] px-4 py-2 justify-start 
         rounded-[0.5rem] text-sm font-normal shadow-none 
-        h-8 lg:50 sm:w-40"
+        h-8 lg:w-64 sm:w-40"
       type="button"
     >
       <input
